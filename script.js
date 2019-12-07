bc18aee4127c88329f0ae35de3c569ac21efb6f2
Ilya <ilyagilash@gmail.com>
2019-12-07 18:21:09 +0700

    1st commit

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+a = 5;
+
+console.log(a);
+let persone = {
+    name: "Alex",
+    age: 34,
+    isMarried: true
+};
+
+let arr = ['plum.png','orange.jpg', 'apple.bmp'];
+console.log(persone.name);
+
+// alert("Hello World!");
+
+//  let answer = +prompt("18?", "да");
+
+//  console.log(typeof(answer));
+
+//  console.log(4 + +"- object");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose= false;
+
+console.log(isChecked && !isClose);
